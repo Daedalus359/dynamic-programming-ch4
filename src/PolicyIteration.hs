f0b2fde6a78f7670059c54f0f1896d8b0b9fb9e8
= <=>
2020-03-03 14:18:49 -0800

    most of the policyImprovement code in place

--- src/PolicyIteration.hs
+++ src/PolicyIteration.hs
@@ -16,8 +16,8 @@ type ValTable s = s -> Value
 initZeros :: (Ord s) => StateSpace s -> ValTable s
 initZeros states = (\s -> fromMaybe vtError $ Map.lookup s $ Map.fromSet (const 0.0) states)
 
-fromMap :: (Ord s) => Map.Map s Value -> ValTable s
-fromMap map = (\s -> fromMaybe vtError $ Map.lookup s map)
+vtFromMap :: (Ord s) => Map.Map s Value -> ValTable s
+vtFromMap map = (\s -> fromMaybe vtError $ Map.lookup s map)
 
 vtError :: Value
 vtError = error "lookup in value table failed"
@@ -28,18 +28,23 @@ type Policy s a = s -> a
 
 --this might ge better in the application specific code
 initPolicy :: (Ord s) => MDP s a -> Policy s a
-initPolicy (MDP states _ af) = (\s -> fromMaybe (error "lookup in policy map failed") $ Map.lookup s $ Map.fromSet firstChoice states)
+initPolicy (MDP states _ af) = (\s -> fromMaybe polError $ Map.lookup s $ Map.fromSet firstChoice states)
   where
     firstChoice s = case (af s) of
       [] -> error "action function returns no options"
       (a:as) -> a
+
+polFromMap :: (Ord s) => Map.Map s a -> Policy s a
+polFromMap map = (\s -> fromMaybe polError $ Map.lookup s map)
+
+polError = error "lookup in policy map failed"
 
 type PolValueUpdate s a = Dynamics s a -> DiscountFactor -> Policy s a -> ValTable s -> s -> Value--for use in step 2 of policy iteration
 
 policyEvaluation :: (Ord s) => Double -> MDPTask s a -> Policy s a -> ValTable s -> ValTable s --returns the values under pi
 policyEvaluation threshold (MDPTask (MDP states dynamics af) gamma) policy valTable = go valTable
   where
-    go vt = let (delta, newVT) = fromMap <$> (foldr (accumValues vt) (0, Map.empty) states) in
+    go vt = let (delta, newVT) = vtFromMap <$> (foldr (accumValues vt) (0, Map.empty) states) in
       if (delta < threshold)
         then newVT
         else go newVT
@@ -52,3 +57,18 @@ policyEvaluation threshold (MDPTask (MDP states dynamics af) gamma) policy valTa
         oldVal = vt state
         newDelta = max delta $ abs $ newVal - oldVal
         newMap = Map.insert state newVal newVT
+
+policyImprovement :: (Ord s, Eq a) => Double -> MDPTask s a -> Policy s a -> ValTable s -> (ValTable s, Policy s a)
+policyImprovement threshold tsk@(MDPTask (MDP states dynamics af) gamma) policy valTable = go policy valTable
+  where
+    go pol vt = let evt = (evaluatedVT pol vt) in let (stable, newPol) = polFromMap <$> (foldr (accumActions evt pol) (True, Map.empty) states) in
+      if stable
+        then (evt, newPol)
+        else go newPol evt
+
+    evaluatedVT = policyEvaluation threshold tsk
+
+    accumActions evt pol state (stableOld, polSoFar) = (stableNew, newPol)
+      where
+        newPol = undefined --need to make sure tie-breaking favors old policy if applicable
+        stableNew = stableOld && ((pol state) == (newPol state))
