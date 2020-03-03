2b6a7706f9ad9e3e4a2ad496a50f2b9adebce014
= <=>
2020-03-03 10:39:43 -0800

    new module split

---
+++ src/MDP.hs
@@ -0,0 +1,15 @@
+module MDP where
+
+import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
+import Data.Monoid
+
+type StateSpace s = Set.Set s
+type Reward = Double
+type Probability = Double
+
+type Dynamics s a = s -> a -> [(s, Reward, Probability)]
+testDynamics :: Dynamics s a -> s -> a -> Bool --lawful dynamics functions should always return a list such that the probabilities sum to 0
+testDynamics dyn s a = (== 1) $ getSum $ foldMap (\(s,r,p) -> Sum p) $ dyn s a
+
+data MDP s a = MDP {states :: StateSpace s, dynamics :: Dynamics s a, actionsF :: s -> [a]}
--- src/PolicyIteration.hs
+++ src/PolicyIteration.hs
@@ -1,19 +1,8 @@
 module PolicyIteration where
 
-import qualified Data.Set as Set
-import qualified Data.Map.Strict as Map
-import Data.Monoid
+import MDP
 
-type StateSpace s = Set.Set s
-type Reward = Double
-type Probability = Double
-
-type Dynamics s a = s -> a -> [(s, Reward, Probability)]
---lawful dynamics functions should always return a list such that the probabilities sum to 0
-testDynamics :: Dynamics s a -> s -> a -> Bool
-testDynamics dyn s a = (== 1) $ getSum $ foldMap (\(s,r,p) -> Sum p) $ dyn s a
-
-data MDP s a = MDP {states :: StateSpace s, dynamics :: Dynamics s a}
+import Data.Map.Strict as Map
 
 type Value = Double
 type ValTable s = Map.Map s Value
