5a0f8e374266944599f4da7aedba048194a891b5
= <=>
2020-03-03 15:44:19 -0800

    core task representation for Jack's Car Rental problem

--- src/JacksRentals.hs
+++ src/JacksRentals.hs
@@ -1,3 +1,14 @@
 module JacksRentals where
 
-test = ""
+import MDP
+
+type EodState = (Int, Int) --how many cars are in locations 1 and 2, respectively, at the end of the day?
+
+type CarMoves = Int --how many cars moved from location 1 to location 2 (negative for other direction)
+
+carMoveCost = 2
+
+businessDynamics :: Dynamics EodState CarMoves --EodState -> CarMoves -> [(EodState, Reward, Probability)]
+businessDynamics eodState carsMoved = undefined
+  where
+    transportCost = abs carsMoved * carMoveCost
