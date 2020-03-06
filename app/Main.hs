c3b7460c874773febde19fe4a38b9068ea93916e
= <=>
2020-03-05 16:57:01 -0800

    more interactive progress in the executable

--- app/Main.hs
+++ app/Main.hs
@@ -9,24 +9,28 @@ import Data.Hashable
 import qualified Data.Set as Set
 
 main :: IO ()
-main = do
+main = polImprovIO vtThreshold jacksTask jacksPolInit jacksVTInit >> return ()
+
+{-  do
   print $ (argMaxByFirst jacksFinalVT eodStates, maximum $ fmap jacksFinalVT $ Set.toList eodStates)
-  foldMap p $ reverse [0 .. maxCarsLot]
+  printPolicy jacksFinalPolicy
   print $ jacksFinalVT (maxCarsLot, maxCarsLot)
 
   print $ fmap jacksFinalPolicy $ ((,) maxCarsLot) <$> [0 .. maxCarsLot]
+-}
 
-p i = print $ fmap jacksFinalPolicy $ ((,) i) <$> [0 .. maxCarsLot]
-
-printPolicy :: (EodState -> CarMoves) -> IO ()
-printPolicy = undefined
-
-polImprovIO :: (Hashable s, Eq s, Eq a) => Double -> MDPTask s a -> Policy s a -> ValTable s -> IO (ValTable s, Policy s a)
-polImprovIO threshold tsk@(MDPTask (MDP states dynamics af) gamma) policy valTable = go (False, valTable, policy, 0)
+printPolicy :: Policy EodState CarMoves -> IO ()
+printPolicy pol = foldMap p $ reverse [0 .. maxCarsLot]
   where
-    go (stable, vt, pol, n) = (>>) (putStrLn ("Iteration " ++ (show n))) $ if (stable)
-      then undefined
-      else undefined
+    p i = print $ fmap pol $ ((,) i) <$> [0 .. maxCarsLot]
+
+polImprovIO :: Double -> MDPTask EodState CarMoves -> Policy EodState CarMoves -> ValTable EodState -> IO (ValTable EodState, Policy EodState CarMoves)
+polImprovIO threshold tsk@(MDPTask (MDP states dynamics af) gamma) policy valTable = go False valTable policy 0
+  where
+    go :: Bool -> ValTable EodState -> Policy EodState CarMoves -> Int -> IO (ValTable EodState, Policy EodState CarMoves)
+    go stable vt pol n = (>>) (putStrLn ("Iteration " ++ (show n))) $ (>>) (printPolicy pol) $ if stable
+      then return (vt, pol)
+      else let (nextStable, nextVT, nextPol) = policyStep threshold tsk pol vt in go nextStable nextVT nextPol (n + 1)
 
 ex_4_1 :: IO Double
 ex_4_1 = undefined
